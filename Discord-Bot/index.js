9fd4d203457317d04b3533f907eeba3951038280
mapletan <29450450+mapletan@users.noreply.github.com>
2018-06-06 01:24:17 -0700

    Add feature to kick members, add roles to members, remove roles from members, and welcome message to the server

--- Discord-Bot/index.js
+++ Discord-Bot/index.js
@@ -14,19 +14,25 @@ var counter3 = 0;
 
 var boolboi = false;
 var arrayMembers = [];
+const newUsers = [];
 // when the client is ready, run this code
 // this event will trigger whenever your bot:
 // - finishes logging in
 // - reconnects after disconnecting
 client.on('ready', () => {
     console.log('Ready!');
+    client.user.setUsername("Derp")
 });
 
-client.on('message', message => {
+client.on('message', async message => {
 	// If the msg does not start with an ! or if msg is from a bot.
 	// Prevents the rest of the func from running
 	//if (!message.content.startsWith(`${prefix}`) || message.author.bot) return;
 	if (message.author.bot) return;
+
+	let messageArray = message.content.split(" ");
+	let args = messageArray.slice(1);
+
 
     //if (message.content === `${prefix}ping`) {
     if (message.content.startsWith(`${prefix}ping`)) {
@@ -113,8 +119,119 @@ client.on('message', message => {
 		var ran = Math.floor(Math.random()*100);
 		message.channel.send(`Your random number is: ${ran}`);
 	}
+	else if (message.content.startsWith(`${prefix}kick`)) {
+
+		let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+   		if(!kUser) return message.channel.send("Can't find user!");
+    	let kReason = args.join(" ").slice(22);
+    	if(!kReason) kReason = "No reason"
+    	if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("No can do pal!");
+    	if(!kUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("That person can't be kicked!");
+
+    	let kickEmbed = new Discord.RichEmbed()
+    	.setDescription("~Kick~")
+    	.setColor("#e56b00")
+    	.addField("Kicked User", `${kUser} with ID ${kUser.id}`)
+    	.addField("Kicked By", `<@${message.author.id}> with ID ${message.author.id}`)
+    	.addField("Kicked In", message.channel)
+    	.addField("Time", message.createdAt)
+    	.addField("Reason", kReason); 
+
+    	/*message.channel.send({
+    		embed: {
+    			author: {
+    				name: client.user.username,
+    				icon_url: client.user.avatarURL
+    			},
+    			title: message.content.substr("!poll ".length),
+    			fields: [{
+    				name: "Kicked User",
+    				value: `${kUser} with ID ${kUser.id}`
+    			},
+    			{
+    				name: "Kicked by",
+    				value: `<@${message.author.id}> with ID ${message.author.id}`
+    			},
+    			{
+    				name: "Kicked In"
+    				//value: "channel"
+    			},
+    			{
+    				name: "Reason",
+    				value: `${kReason}`
+    			}
+    			],
+    			timestamp: new Date(),
+    			
+     		}
+     	})*/
+
+
+    	let kickChannel = message.guild.channels.find(`name`, "bot-spam");
+    	if(!kickChannel) return message.channel.send("Can't find incidents channel.");
+
+    	message.guild.member(kUser).kick(kReason);
+	    kickChannel.send(kickEmbed);
+
+    	return;
+
+	}
+	else if (message.content.startsWith(`${prefix}addrole`)) {
+
+		let roleName = args[1];
+		let role = message.guild.roles.find("name", roleName);
+		if(!role) return message.channel.send("That role does not exist!");
+		let member = message.mentions.members.first() || message.guild.members.get(args[0]);
+
+		member.addRole(role).catch(console.error);
+	}
+	else if (message.content.startsWith(`${prefix}bronze`)) {
+
+		let roleName = args[1];
+		let role = message.guild.roles.find("name", roleName);
+		if(!role) return message.channel.send("That role already exists!");
+		let member = message.mentions.members.first();
+
+		member.removeRole(role).catch(console.error);
+
+	}
+	else if (message.content.startsWith(`${prefix}createrole`)) {
+
+		let roleName = args[1];
+		let role = message.guild.roles.find("name", roleName);
+		if(!role) return message.channel.send("That role already exists!");
+
+		message.guild.createRole({
+
+			data: {
+				name: '${roleName}',
+				hoist: true,
+				mentionable: true,
+			},
+		})
+
+	}
+
 });
 
+client.on("guildMemberAdd", (member) => {
+
+	const guild = member.guild;
+	if(!newUsers[guild.id]) newUsers[guild.id] = new Discord.Collection();
+	newUsers[guild.id].set(member.id, member.user);
+
+	const userlist = newUsers[guild.id].map(u => u.toString()).join(" ");
+	guild.channels.find("name", "welcome").send("Welcome to the server, " + userlist + "!");
+	newUsers[guild.id].clear();
+
+});
+
+client.on("guildMemberRemove", (member) => {
+	const guild = member.guild;
+	guild.channels.find("name", "welcome").send("So sad to see you go, @" + member.id);
+	if (newUsers[guild.id].has(member.id)) newUsers.delete(member.id);
+
+});
 // login to Discord with your app's token
 //client.login(config.token);
 client.login(token);
